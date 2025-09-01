5f353ae0ea2b355466d03c454c3d54d6cc61273a
Len <len@netblaze.net>
2025-09-01 14:58:04 +0800

    fix: 修改

--- src/service/api/user.ts
+++ src/service/api/user.ts
@@ -1,36 +1,36 @@
 import { request } from '../request';
-import { type CommListPlader, type CommIdPlader} from "./typescript/comm-plader"
+import { type CommIdPlader, type CommListPlader } from './typescript/comm-plader';
 
 // 登录
 export interface LoginPlader {
-    username: string,
-    password: string
+  username: string;
+  password: string;
 }
 
-export interface AuditPlader extends CommIdPlader{
-  status: number
+export interface AuditPlader extends CommIdPlader {
+  status: number;
 }
 
 export interface KycSearchPlader extends CommIdPlader {
-  filter?: string,//搜索条件（邮箱、......）
-  status?: string
+  filter?: string; // 搜索条件（邮箱、......）
+  status?: string;
 }
 
 export interface UserListPlader extends CommIdPlader {
-  status?: string,
-  mobile?: string //手机号
+  status?: string;
+  mobile?: string; // 手机号
 }
 
 export interface UserLevelRequest {
-  id?: number,
-  slug?: string, // 等级标识
-  name_en?: string, // 英文名称
-  name_es?: string, // 西班牙名称
-  name_jp?: string, // 日语名称
-  name_kr?: string, // 韩语名称
-  despoit_money?: number, // 充值金额
-  high_yield_rate?: number, // 高收益比例
-  high_yield_amount_rate?: number // 高收益金额比例
+  id?: number;
+  slug?: string; // 等级标识
+  name_en?: string; // 英文名称
+  name_es?: string; // 西班牙名称
+  name_jp?: string; // 日语名称
+  name_kr?: string; // 韩语名称
+  despoit_money?: number; // 充值金额
+  high_yield_rate?: number; // 高收益比例
+  high_yield_amount_rate?: number; // 高收益金额比例
 }
 
 /**
@@ -130,65 +130,64 @@ export interface ResetTradePasswordRequest {
  * @returns
  */
 export function fetchGetUserList(params: UserSearchParams) {
-  return request({url:'/api/admin/users/user/list', params})
+  return request({ url: '/api/admin/users/user/list', params });
 }
 // 用户详情
 export function fetchGetUserInfo(params: CommIdPlader) {
-  return request({url:'/api/admin/users/user/detail', params})
+  return request({ url: '/api/admin/users/user/detail', params });
 }
 
 // 用户更新
 export function fetchUpdateUser(data: UserUpdateRequest) {
-  return request({url:'/api/admin/users/user/update', method: 'post', data})
+  return request({ url: '/api/admin/users/user/update', method: 'post', data });
 }
 
 // 重置密码
 export function fetchResetPassword(data: ResetPasswordRequest) {
-  return request({url:'/api/admin/users/user/reset-password', method: 'post', data})
+  return request({ url: '/api/admin/users/user/reset-password', method: 'post', data });
 }
 
 // 重置交易密码
 export function fetchResetTradePassword(data: ResetTradePasswordRequest) {
-  return request({url:'/api/admin/users/user/reset-trade-password', method: 'post', data})
+  return request({ url: '/api/admin/users/user/reset-trade-password', method: 'post', data });
 }
 
-
-/*KYC管理*/
+/* KYC管理 */
 // 审核
 export function UserKycAudit(data: AuditPlader) {
-  return request({url: '/api/admin/users/kyc/audit', data})
+  return request({ url: '/api/admin/users/kyc/audit', data, method: 'post' });
 }
 // 详情
 export function UserKycInfo(params: CommIdPlader) {
-  return request ({url:'/api/admin/users/kyc/detail', params})
+  return request({ url: '/api/admin/users/kyc/detail', params });
 }
 // 列表
 export function UserKycList(params: any) {
-  return request ({url:'/api/admin/users/kyc/list', params})
+  return request({ url: '/api/admin/users/kyc/list', params });
 }
 
-/*用户等级管理*/
+/* 用户等级管理 */
 // 用户等级列表
 export function UserLevelList(params: any) {
-  return request({url: '/api/admin/users/level/list', params})
+  return request({ url: '/api/admin/users/level/list', params });
 }
 
 // 添加用户等级
 export function UserLevelAdd(data: UserLevelRequest) {
-  return request({url: '/api/admin/users/level/create', method: 'post', data})
+  return request({ url: '/api/admin/users/level/create', method: 'post', data });
 }
 
 // 修改用户等级
 export function UserLevelUpdate(data: UserLevelRequest) {
-  return request({url: '/api/admin/users/level/update', method: 'post', data})
+  return request({ url: '/api/admin/users/level/update', method: 'post', data });
 }
 
 // 删除用户等级
 export function UserLevelDelete(params: CommIdPlader) {
-  return request({url: '/api/admin/users/level/delete', params})
+  return request({ url: '/api/admin/users/level/delete', params });
 }
 
 // 用户等级详情
 export function UserLevelDetail(params: CommIdPlader) {
-  return request({url: '/api/admin/users/level/detail', params})
+  return request({ url: '/api/admin/users/level/detail', params });
 }
