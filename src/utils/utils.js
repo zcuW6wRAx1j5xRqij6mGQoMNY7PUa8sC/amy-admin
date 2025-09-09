4f39be223551ceafffcbf61de31cee6b134b35a9
milo <omnimiloo@gmail.com>
2025-09-10 06:50:46 +0700

    fix: change url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -15,7 +15,7 @@ export const encodedParams = (obj = {}) => {
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
     .join('&');
 };
-export const baseUrl = 'https://s3.infantry5.com';
+export const baseUrl = 'https://s3.amy.finance';
 
 export function setBaseUrl(url) {
   if (!url) {
